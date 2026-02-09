d9a6c97f87f16416c519f5b64bf542af06b926f2
Maryna Rozhdestvenska <rozhd.buro@gmail.com>
2026-02-09 20:04:10 +0200

    Lesson 7: remove lesson-5 folder and tfvars example

--- lesson-5/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "maro-lesson-5-tfstate-12345"
-    key            = "lesson-5/terraform.tfstate"
-    region         = "eu-central-1"
-    dynamodb_table = "maro-lesson-5-tf-locks"
-    encrypt        = true
-  }
-}
